8591ce0ec614a5889567e9ec2a3f8a9237820dd4
master <rudolifrudolif@gmail.com>
2020-05-30 14:36:53 +0300

    accardeon

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
+//------------------------------acardeon---------------------------
+  $(".block__content").slideUp("slow");
+  $(".block--active .block__content").slideDown("slow");
+
+  $(".block__header").on("click", function(){
+    if ($(this).parent().hasClass('block--active')) {
+      $(this).parent().removeClass('block--active');
+      $(".block__content").slideUp("slow");
+    }
+    else {
+      $(".block--active .block__content").slideUp("slow");
+      $(".block--active").removeClass('block--active');
+      $(this).parent().addClass('block--active');
+      $(".block--active .block__content").slideDown("slow");
+    }
+  });
+
 //------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $('body').toggleClass('no-scroll');
